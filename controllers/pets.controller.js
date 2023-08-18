06192fbd066c7d228cd7b8ffe1c55256038c1f79
Dinuka <shehan.shyaminda@gmail.com>
2023-08-18 09:21:09 +0530

    changes in pets controller

--- controllers/pets.controller.js
+++ controllers/pets.controller.js
@@ -3,40 +3,56 @@ const {v4: uuidv4} = require('uuid');
 const jwt = require("jsonwebtoken");
 
 exports.updateSelfLocation = (req, res) => {
-    db.user.findOneAndUpdate({
+    db.user.findOneAndUpdate(
+        {
             userId: req.body.userId,
             'pets.petId': req.body.petId
-        }, {
+        },
+        {
             $set: {
                 'pets.$.petLongitude': req.body.petLongitude,
                 'pets.$.petLatitude': req.body.petLatitude
             }
         },
-        {new: true}).then(r => {
+        { new: true }
+    ).then(r => {
         if (r != null) {
             const locationHistory = {
                 dateTime: Date.now(),
                 longitude: req.body.petLongitude,
-                latitude: req.body.petLatitude,
+                latitude: req.body.petLatitude
             };
 
-            db.user.findOneAndUpdate({
+            db.user.findOneAndUpdate(
+                {
                     userId: req.body.userId,
                     'pets.petId': req.body.petId
-                }, {
+                },
+                {
                     $push: {
-                        locationHistory: locationHistory
+                        'pets.$.locationHistory': locationHistory
                     }
-                }, {new: true}).then(r => {
+                },
+                { new: true }
+            ).then(r => {
                 if (r != null) {
-                    console.log(`new pet registered ${r}`);
-                    res.status(200).send({status: true, data: r})
+                    console.log(`New location added to history: ${r}`);
+                    res.status(200).json({
+                        status: true,
+                        data: r
+                    });
                 } else {
-                    res.status(400).send({status: false, message: 'Update Error'});
+                    res.status(400).json({
+                        status: false,
+                        message: 'Location History Update Error'
+                    });
                 }
-            })
+            });
         } else {
-            res.status(400).send({status: false, message: 'Update Error'});
+            res.status(400).json({
+                status: false,
+                message: 'Pet Location Update Error'
+            });
         }
-    })
-}
+    });
+};
